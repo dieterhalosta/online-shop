b4818adf5a1836dccc28cf06dda74dfc05a625e9
Dieter <dieter.halosta@yahoo.com>
2020-06-29 20:18:57 +0300

    Studied manually defined queries and JPQL.

--- src/main/java/org/fastttrackit/onlineshop/persistance/ProductRepository.java
+++ src/main/java/org/fastttrackit/onlineshop/persistance/ProductRepository.java
@@ -4,6 +4,7 @@ import org.fastttrackit.onlineshop.domain.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
@@ -11,5 +12,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product>findByNameContainingAndQuantityGreaterThanEqual(String partialName, int minimumQuantity, Pageable pageable);
 
-
+    //JPQL syntax
+    @Query(value = "SELECT product FROM Product product WHERE (:partialName IS null OR product.name = :partialName) AND (:minimumQuantity IS null OR product.quantity >= :minimumQuantity)")
+    Page<Product> findByOptionalCriteria(String partialName, Integer minimumQuantity, Pageable pageable);
 }
